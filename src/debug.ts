965a52d3bd49d29caaabdf04950a7e6e8b58f07e
Alex Dima <alexdima@microsoft.com>
2016-09-09 19:38:06 +0200

    add debug.ts

---
+++ src/debug.ts
@@ -0,0 +1,7 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+'use strict';
+
+export const CAPTURE_METADATA = !!process.env['VSCODE_TEXTMATE_DEBUG'];
+export const IN_DEBUG_MODE = !!process.env['VSCODE_TEXTMATE_DEBUG'];
