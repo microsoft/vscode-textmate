9247e5af1691e2967fdd84f9657495b2513e551c
Martin Aeschlimann <martinae@microsoft.com>
2019-11-12 22:44:22 +0100

    update onigasm (fix php issue)

---
+++ test-cases/onigtests/fixtures/php-onigasm-issue17.php
@@ -0,0 +1,14 @@
+<?php
+  // Declare the variable 'string' and assign it a value.
+  // The <br> is the HTML equivalent to a new line.
+  $string = 'Hello World!<br>';
+
+  // You can echo the variable, similar to the way you would echo a string.
+  echo $string;
+
+  // You could also use print.
+  print $string;
+
+  // Or, if you are familiar with C, printf can be used too.
+  printf('%s', $string);
+?>
