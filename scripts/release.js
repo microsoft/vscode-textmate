ebf171759ede39c6cb56a04317b79ae705782e5f
Alex Dima <alexdima@microsoft.com>
2020-06-29 14:46:42 +0200

    Switch to webpack to create a minified bundle

--- scripts/release.js
+++ scripts/release.js
@@ -1,64 +1,12 @@
-var cp = require('child_process');
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-cp.execSync('node node_modules/typescript/bin/tsc', {
-	cwd: path.join(__dirname, '..')
-});
-
-var OUT_FOLDER = path.join(__dirname, '../out');
-var RELEASE_FOLDER = path.join(__dirname, '../release');
-
+const OUT_FOLDER = path.join(__dirname, '../out');
+const RELEASE_FOLDER = path.join(__dirname, '../release');
+``
 if (!fs.existsSync(RELEASE_FOLDER)) {
 	fs.mkdirSync(RELEASE_FOLDER);
 }
 
-var sources = [
-	'utils.js',
-	'theme.js',
-	'matcher.js',
-	'debug.js',
-	'json.js',
-	'plist.js',
-	'grammarReader.js',
-	'rule.js',
-	'grammar.js',
-	'registry.js',
-	'main.js'
-].map(function(sourceFile) {
-	var name = './' + sourceFile.replace(/\.js$/, '');
-	var sourcePath = path.join(OUT_FOLDER, sourceFile);
-	var sourceContents = fs.readFileSync(sourcePath).toString();
-
-	return [
-		"$load('" + name + "', function(require, module, exports) {",
-		sourceContents,
-		"});"
-	].join('\n');
-});
-
-var all = [];
-all.push(fs.readFileSync(path.join(OUT_FOLDER, '_prefix.js')).toString());
-all = all.concat(sources);
-
-const result = `
-(function (factory) {
-    if (typeof exports === 'object' && typeof module === 'object') {
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    }
-})(function () {
-
-${stripSourceMaps(all.join('\n'))}
-
-return $map['./main'].exports;;
-});`;
-
-fs.writeFileSync(path.join(RELEASE_FOLDER, 'main.js'), result);
 fs.writeFileSync(path.join(RELEASE_FOLDER, 'main.d.ts'), fs.readFileSync(path.join(OUT_FOLDER, 'main.d.ts')));
 fs.writeFileSync(path.join(RELEASE_FOLDER, 'types.d.ts'), fs.readFileSync(path.join(OUT_FOLDER, 'types.d.ts')));
-
-function stripSourceMaps(str) {
-	return str.replace(/\/\/# sourceMappingURL[^\n]+/gm, '');
-}
