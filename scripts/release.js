98b4c2ff8b8d6e84075c7ee429cbc4fbb53d0ebe
Henning Dieterichs <hdieterichs@microsoft.com>
2022-05-09 10:18:24 +0200

    Uses webpack copy plugin to include all d.ts files in the release.

--- scripts/release.js
+++ scripts/release.js
@@ -3,10 +3,7 @@ const path = require('path');
 
 const OUT_FOLDER = path.join(__dirname, '../out');
 const RELEASE_FOLDER = path.join(__dirname, '../release');
-``
+
 if (!fs.existsSync(RELEASE_FOLDER)) {
 	fs.mkdirSync(RELEASE_FOLDER);
 }
-
-fs.writeFileSync(path.join(RELEASE_FOLDER, 'main.d.ts'), fs.readFileSync(path.join(OUT_FOLDER, 'main.d.ts')));
-fs.writeFileSync(path.join(RELEASE_FOLDER, 'types.d.ts'), fs.readFileSync(path.join(OUT_FOLDER, 'types.d.ts')));
